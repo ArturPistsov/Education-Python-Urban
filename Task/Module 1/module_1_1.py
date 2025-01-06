856767cc7f87ac63469c849c86cf3eb0fcb7bbac
ArturPistsov <artur.piscov@gmail.com>
2025-01-06 11:55:14 +0500

    new commit

---
+++ Task/Module 1/module_1_1.py
@@ -0,0 +1,18 @@
+#1st program
+a=(9**0.5)*5
+print(a)
+
+#2st program
+b=bool(9.99>9.98 and 1000!=1000.1)
+print(b)
+
+#3st program
+c=2*2+2
+d=2*(2+2)
+print(c)
+print(d)
+print(c==d)
+
+#4st program
+e=int((float('123.456')*10)%10)
+print(e)
