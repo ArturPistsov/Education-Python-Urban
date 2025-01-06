f56d97d7c6e9348cbfd1e0214f3f552404e759a0
ArturPistsov <artur.piscov@gmail.com>
2025-01-06 12:10:18 +0500

    new commit

---
+++ Task/Module 1/module_1_2.py
@@ -0,0 +1,10 @@
+# Количество выполненных ДЗ
+tasks_count = 12
+# Количество затраченных часов
+time_all_tasks = 1.5
+# Название курса
+course_name = 'Python'
+# Время на одно задание
+time_one_tasks = time_all_tasks / tasks_count
+
+print('Курс:',course_name,' всего задач:',tasks_count,' затрачено часов:',time_all_tasks,' среднее время выполнения:',time_one_tasks)
